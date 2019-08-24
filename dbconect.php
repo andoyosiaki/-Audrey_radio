78f1f074595b64b7e89b28b7d3db1fb9ec09d0ff
andouyosiaki <yutori4dayo@gmail.com>
2019-08-24 18:53:15 +0900

    修正

--- dbconect.php
+++ dbconect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-     $db = new PDO('mysql:dbname=test;host=localhost;charset=utf8','root', 'root');
+     $db = new PDO('mysql:dbname=youtube;host=localhost;charset=utf8','root', 'root');
 }catch (PDOExeception $e){
   echo "DB接続エラー:". $e->getMassage();
 }
